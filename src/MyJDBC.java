1ecdaac9887f6f3150e8c21341cd07af05c5778b
Breno Chaves <103227487+Brenoch@users.noreply.github.com>
2025-06-04 00:32:39 -0300

    Add files via upload

--- src/MyJDBC.java
+++ src/MyJDBC.java
@@ -1,9 +1,30 @@
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class MyJDBC {
     public static void main(String[] args){
-        Connection connection = DriverManager.getConnection(
-         "jdbc:mysql://"
-        );
+        try{
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://192.168.100.6:3306/avaliacao_restaurantes",
+                    "programador",
+                    "1234teste-"
+            );
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM avaliacoes");
+
+            while (resultSet.next()){
+                System.out.println("ID: " + resultSet.getInt("id"));
+                System.out.println("Usuário ID: " + resultSet.getInt("usuario_id"));
+                System.out.println("Restaurante ID: " + resultSet.getInt("restaurante_id"));
+                System.out.println("Nota: " + resultSet.getInt("nota"));
+                System.out.println("Comentário: " + resultSet.getString("comentario"));
+                System.out.println("Data: " + resultSet.getDate("data_avaliacao"));
+                System.out.println("--------------------------");
+            }
+            
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
