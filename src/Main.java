868cca1883da99a0fc914181a18ea5c75b66ad68
ThiagoNeves123 <143027214+ThiagoNeves123@users.noreply.github.com>
2025-06-06 18:05:13 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,25 @@ import util.AvaliacaoAtendimento;
 import util.AvaliacaoComida;
 import util.Cliente;
 import util.AvaliacaoLocalizacao;
-
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner aval = new Scanner(System.in);
 
-        Scanner notatst = new Scanner(System.in);
-        Scanner login = new Scanner(System.in);
+        Cliente armando = new Cliente(19324295, "Armando", "abc", "12345");
+        armando.autenticacao();
 
-        Cliente jose = new Cliente(19324295, "Armando", "Armando@gmail.com", "12356");
+        AvaliacaoAtendimento atendimento = new AvaliacaoAtendimento(5.0f);
+        AvaliacaoComida comida = new AvaliacaoComida(4.7f);
+        AvaliacaoAmbiente ambiente = new AvaliacaoAmbiente(5.0f);
+        AvaliacaoLocalizacao local = new AvaliacaoLocalizacao(4.2f);
 
+        System.out.println("Notas: ");
+        System.out.println("Atendimento: " + atendimento.getNotaAtendimento());
+        System.out.println("Comida: " + comida.getNotaComida());
+        System.out.println("Ambiente: " + ambiente.getNotaAmbiente());
+        System.out.println("Localização: " + local.getNotaLocalizacao());
 
-        System.out.println("Login realizado com sucesso!");
-            System.out.print("Dê uma nota para o atendimento: ");
-            float notaAtendimento = notatst.nextFloat();
-
-            System.out.print("Dê uma nota para a comida: ");
-            float notaComida = notatst.nextFloat();
-
-            System.out.print("Dê uma nota para o ambiente: ");
-            float notaAmbiente = notatst.nextFloat();
-
-            System.out.println("Dê uma nota para o local: ");
-            float notaLocal = notatst.nextFloat();
-
-            AvaliacaoAtendimento atendimento = new AvaliacaoAtendimento(notaAtendimento);
-            AvaliacaoComida comida = new AvaliacaoComida(notaComida);
-            AvaliacaoAmbiente ambiente = new AvaliacaoAmbiente(notaAmbiente);
-            AvaliacaoLocalizacao local = new AvaliacaoLocalizacao(notaLocal);
-
-
-            Cliente.avaliar(atendimento, comida, ambiente, local);
-        }
     }
+}
